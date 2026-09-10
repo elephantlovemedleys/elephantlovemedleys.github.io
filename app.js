dc6e6e2699375593207c8fac2d48e08a1c97fb44
elephantlovemedleys <cecelecsin@gmail.com>
2026-09-10 14:11:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ toggles.forEach((toggle) => {
     toggle.parentNode.classList.toggle("active");
   });
 });
+
+const search = () => {
+    const searchbox = document.getElementById("search-item").value.toUpperCase();
+    const product = document.querySelectorAll(".faq");
+
+    for (let i = 0; i < product.length; i++) {
+        const matches = product[i].querySelectorAll('.faq-title, .faq-subtitle, .faq-text');
+        let textvalue = "";
+        matches.forEach(el => textvalue += el.textContent + " ");
+
+        if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+            product[i].style.display = "";
+        } else {
+            product[i].style.display = "none";
+        }
+    }
+}
